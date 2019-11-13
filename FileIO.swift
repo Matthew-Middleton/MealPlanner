f37cde264f2d6fd208f36036b0f420a81c5a2e21
Matthew Middleton <mattmidd6@gmail.com>
2019-11-12 21:59:53 -0500

    Added class template for file i/o interaction with links discussing how to handle this in swift

---
+++ FileIO.swift
@@ -0,0 +1,14 @@
+public class FileIO
+{
+	public init()
+	{
+	}
+  
+	/*https://medium.com/swift-india/saving-data-in-ios-part-2-a8c9f810d5c
+	
+	https://www.techotopia.com/index.php/Working_with_Files_in_Swift_on_iOS_8*/
+	public write_to(fileName: String)
+	{
+		
+	}
+}
