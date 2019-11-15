cd7b3345cc17d7b11b92dfe19bddc3d1d960d088
Matthew Middleton <mattmidd6@gmail.com>
2019-11-14 19:09:20 -0500

    Added example and sources of how to use the functions

--- FileIO.swift
+++ FileIO.swift
@@ -4,6 +4,8 @@ public class FileIO
 {
 	/*https://medium.com/swift-india/saving-data-in-ios-part-2-a8c9f810d5c
 	
+	https://www.objc.io/blog/2018/02/13/string-to-data-and-back/
+	
 	https://www.iosapptemplates.com/blog/ios-development/data-persistence-ios-swift
 	
 	https://www.techotopia.com/index.php/Working_with_Files_in_Swift_on_iOS_8*/
@@ -18,11 +20,18 @@ public class FileIO
 
     let fileManager: FileManager
 	
-    init(fileManager: FileManager = .default)
+    public init(fileManager: FileManager = .default)
 	{
         self.fileManager = fileManager
     }
 	
+	/*Example usage of save:
+	import Foundation
+	var io = FileIO()
+	var name = "potato"
+	var info: Data? = name.data(using: .utf8)
+	try io.save(fileNamed:"wd", data:info!)
+	*/
     public func save(fileNamed: String, data: Data) throws
 	{
         guard let url = makeURL(forFileNamed: fileNamed)
@@ -43,15 +52,6 @@ public class FileIO
             debugPrint(error)
             throw ioError.writingFailed
         }
-    }
-    private func makeURL(forFileNamed fileName: String) -> URL?
-	{
-        guard let url = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
-		else
-		{
-            return nil
-        }
-        return url.appendingPathComponent(fileName)
     }
 	
 	public func read(fileNamed: String) throws -> Data
@@ -75,6 +75,15 @@ public class FileIO
 			debugPrint(error)
 			throw ioError.readingFailed
 		}
-   }
-
+	}
+	
+	private func makeURL(forFileNamed fileName: String) -> URL?
+	{
+        guard let url = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+		else
+		{
+            return nil
+        }
+        return url.appendingPathComponent(fileName)
+    }
 }
